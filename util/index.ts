0c549485bb800d1ac1ae776a65b2769bd066b825
Shuruhatik <72819916+Shuruhatik@users.noreply.github.com>
2023-08-19 18:49:25 +0300

    Delete util directory

--- util/index.ts
+++
@@ -1,7 +0,0 @@
-import { Collection } from "./Collection.js"
-import { ElementInData } from "./ElementInData.js"
-import { Action } from "./Action.js"
-import { Table } from "./Table.js"
-import { Database } from "./Database.js"
-
-export { Collection, Database, ElementInData, Action, Table }
